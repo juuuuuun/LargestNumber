2373b40511a2fcf25e56dd7f9b8951c381df4937
Jun Oh <kyungjunjoh@gmail.com>
2018-12-28 01:13:04 -0800

    Extracted the largest number algorithm to a function
    Added sample arrays from the assignment page and printed the return value of the function as test

--- LargestNumber/main.m
+++ LargestNumber/main.m
@@ -8,24 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
+static NSNumber* largestNumber(NSArray *numbers) {
+    // Initialize the largest number to be the first element of the array
+    NSNumber* largest = numbers[0];
+    
+    // Loop through all the elements of the array
+    for(NSNumber* number in numbers) {
+        // If the largest is smaller than the number being iterated over, replace the largest with the current number
+        if( [largest doubleValue] < [number doubleValue]) {
+            largest = number;
+        }
+    }
+    return largest;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        // Sample array
-        NSArray* numbers = @[@2, @3, @7, @5, @6];
+        // Sample arrays
+        NSArray* sampleNumbers = @[@2, @"eight", @"4", @7, @5, @6];
+        NSArray* sampleArray1 = @[@3, @7, @6, @8];
+        NSArray* sampleArray2 = @[@44, @5, @6];
         
-        // Initialize the largest number to be the first element of the array
-        NSNumber* largest = numbers[0];
-        
-        // Loop through all the elements of the array
-        for(NSNumber* number in numbers) {
-            // If the largest is smaller than the number being iterated over, replace the largest with the current number
-            if( [largest doubleValue] < [number doubleValue]) {
-                largest = number;
-            }
-            NSLog(@"%@", number);
-        }
-        NSLog(@"%@", largest);
+        NSLog(@"%@", largestNumber(sampleNumbers));
+        NSLog(@"%@", largestNumber(sampleArray1));
+        NSLog(@"%@", largestNumber(sampleArray2));
     }
     return 0;
 }
